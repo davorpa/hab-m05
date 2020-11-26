16d31fbf28187cf16621897b083c6969083c308e
davorpa <davorpatech@gmail.com>
2020-11-26 14:38:53 +0100

    🚧 DDBB DDL Hollywood Oscar prizes. Tabla Relación Film-ProductionData

--- hollywood.ddbb.0000.sql
+++ hollywood.ddbb.0000.sql
@@ -14,7 +14,7 @@ CREATE TABLE `Prize` (
     CONSTRAINT `prize_pk` PRIMARY KEY(`id`),
     CONSTRAINT `title_uniq` UNIQUE (`title`),
     -- restringimos a dato BIT
-    CONSTRAINT `great_category_boolck` CHECK (`great_category` = 0 OR `great_category` = 1)
+    CONSTRAINT `great_category_bool_ck` CHECK (`great_category` = 0 OR `great_category` = 1)
 );
 
 DROP TABLE IF EXISTS `Film`;
@@ -33,6 +33,16 @@ CREATE TABLE `Genre` (
     CONSTRAINT `name_uniq` UNIQUE (`name`)
 );
 
+DROP TABLE IF EXISTS `ProductionData`;
+CREATE TABLE `ProductionData` (
+	`id`				BIGINT UNSIGNED NOT NULL AUTO_INCREMENT				COMMENT 'Identificador',
+    `producer_company`	VARCHAR(255) NOT NULL								COMMENT 'Nombre de la productora encargada de la película',
+	`cost`				DECIMAL(12,2) NOT NULL								COMMENT 'Coste de dinero asociado a la producción',
+    `end_production_date` TIMESTAMP NOT NULL								COMMENT 'Fecha en la que finalizó',
+    CONSTRAINT `production_data_pk` PRIMARY KEY(`id`),
+    CONSTRAINT `production_cost_positive_ck` CHECK (`cost` >= 0)
+);
+
 
 # 
 # RELACIÓN (N:M) PREMIOS QUE GANÓ UNA PELÍCULA
@@ -43,14 +53,14 @@ CREATE TABLE `FilmPrizes` (
 	`id_film`				BIGINT UNSIGNED NOT NULL							COMMENT 'Identificador de película',
     `votes`					INT UNSIGNED DEFAULT 0								COMMENT 'Número de votos recibidos',
     `year`					SMALLINT UNSIGNED NOT NULL							COMMENT 'Año de adquisición',
-    CONSTRAINT `filmprizes_pk` PRIMARY KEY(`id_film`, `id_prize`),
-    CONSTRAINT `id_prize_fk` FOREIGN KEY(`id_prize`) REFERENCES Prize(`id`) 
+    CONSTRAINT `filmprize_pk` PRIMARY KEY(`id_film`, `id_prize`),
+    CONSTRAINT `filmprize_id_prize_fk` FOREIGN KEY(`id_prize`) REFERENCES `Prize`(`id`) 
 			ON DELETE NO ACTION
             ON UPDATE NO ACTION,
-    CONSTRAINT `id_film_fk` FOREIGN KEY(`id_film`) REFERENCES Film(`id`)
+    CONSTRAINT `filmprize_id_film_fk` FOREIGN KEY(`id_film`) REFERENCES `Film`(`id`)
 			ON DELETE NO ACTION
             ON UPDATE NO ACTION,
-    CONSTRAINT `year_greather_than_ck1` CHECK (year > 1700)
+    CONSTRAINT `filmprize_year_gt_ck` CHECK (`year` > 1700)
 );
 
 # 
@@ -60,11 +70,27 @@ DROP TABLE IF EXISTS `FilmGenres`;
 CREATE TABLE `FilmGenres` (
 	`id_film`				BIGINT UNSIGNED NOT NULL							COMMENT 'Identificador de película',
 	`id_genre`				INT UNSIGNED NOT NULL								COMMENT 'Identificador de género',
-    CONSTRAINT `genre_pk` PRIMARY KEY(`id_film`, `id_genre`),
-    CONSTRAINT `id_film_fk` FOREIGN KEY(`id_film`) REFERENCES Film(`id`)
+    CONSTRAINT `filmgenre_pk` PRIMARY KEY(`id_film`, `id_genre`),
+    CONSTRAINT `filmgenre_id_film_fk` FOREIGN KEY(`id_film`) REFERENCES `Film`(`id`)
 			ON DELETE NO ACTION
             ON UPDATE NO ACTION,
-    CONSTRAINT `id_genre_fk` FOREIGN KEY(`id_genre`) REFERENCES Genre(`id`)
+    CONSTRAINT `filmgenre_id_genre_fk` FOREIGN KEY(`id_genre`) REFERENCES `Genre`(`id`)
+			ON DELETE NO ACTION
+            ON UPDATE NO ACTION
+);
+
+# 
+# RELACIÓN (0:N) DATOS DE PRODUCCIÓN QUE POSEE UNA PELÍCULA
+#
+DROP TABLE IF EXISTS `FilmProductionData`;
+CREATE TABLE `FilmProductionData` (
+	`id_film`				BIGINT UNSIGNED NOT NULL							COMMENT 'Identificador de película',
+	`id_genre`				INT UNSIGNED NOT NULL								COMMENT 'Identificador de género',
+    CONSTRAINT `filmproduction_pk` PRIMARY KEY(`id_film`, `id_genre`),
+    CONSTRAINT `filmproduction_id_film_fk` FOREIGN KEY(`id_film`) REFERENCES `Film`(`id`)
+			ON DELETE NO ACTION
+            ON UPDATE NO ACTION,
+    CONSTRAINT `filmproduction_id_genre_fk` FOREIGN KEY(`id_genre`) REFERENCES `Genre`(`id`)
 			ON DELETE NO ACTION
             ON UPDATE NO ACTION
 );
